67864a1fc05ea992f3335fa903432f523fc3a802
Lars Vonk <larserikvonk@outlook.com>
2025-11-29 22:54:03 +0100

    chore: moved products up above blogs

--- src/components/homepage/ProductsPreview.jsx
+++ src/components/homepage/ProductsPreview.jsx
@@ -8,7 +8,7 @@ const { products } = info;
 export default function ProductsPreview() {
 	return (
 		<section id="products" class="flex items-start justify-between flex-col sm:flex-row dark:text-white">
-			<h2 class="w-[15rem] sm:mt-16 mt-24 uppercase sm:text-2xl text-3xl font-semibold">Our services</h2>
+			<h2 class="w-[15rem] sm:mt-16 mt-24 uppercase sm:text-2xl text-3xl font-semibold">Services</h2>
 
 			<div class="w-full grid md:grid-cols-2 grid-cols-1 gap-4">
 				<For each={products}>{product => <ProductCard product={product} />}</For>
