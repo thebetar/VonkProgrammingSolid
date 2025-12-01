7b7062732ede6e8bea01d2252f6c62fddc6bffc0
Lars Vonk <larserikvonk@outlook.com>
2025-12-01 20:34:20 +0100

    fix: products page fix

--- src/components/products/ProductsPreview.jsx
+++ src/components/products/ProductsPreview.jsx
@@ -8,7 +8,7 @@ const { products } = info;
 export default function ProductsPreview() {
 	return (
 		<section id="products" class="dark:text-white">
-			<h2 class="sm:mt-16 mt-24 uppercase sm:text-2xl text-3xl font-semibold mx-auto">Our services</h2>
+			<h2 class="sm:mt-16 mt-24 uppercase sm:text-2xl text-3xl font-semibold mx-auto w-fit">Our services</h2>
 
 			<div class="w-full grid md:grid-cols-2 grid-cols-1 gap-4">
 				<For each={products}>{product => <ProductCard product={product} />}</For>
