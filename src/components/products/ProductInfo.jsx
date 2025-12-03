34fd961f66c7c06e604e9da3336eeb4452e85730
Lars Vonk <larserikvonk@outlook.com>
2025-12-03 16:51:06 +0100

    chore: small styling fix for product info

--- src/components/products/ProductInfo.jsx
+++ src/components/products/ProductInfo.jsx
@@ -1,9 +1,8 @@
 import { Title, Meta } from '@solidjs/meta';
-import { ProductButton } from './ProductButton';
 
-export default function ProductInfo({ title, logo, description, descriptionShort, contactLink, productLink }) {
+export default function ProductInfo({ title, logo, description, descriptionShort }) {
 	return (
-		<div className="max-w-3xl mx-auto flex flex-col items-center">
+		<div className="max-w-3xl mx-auto flex flex-col items-center gap-4">
 			<Title>VonkProgramming - {title}</Title>
 			<Meta name="description" content={descriptionShort || `Learn more about ${title} at VonkProgramming.`} />
 
@@ -26,12 +25,13 @@ export default function ProductInfo({ title, logo, description, descriptionShort
 						'@type': 'Brand',
 						name: 'VonkProgramming',
 					},
-					url: `https://vonkprogramming.nl${productLink}`,
+					url: `https://vonkprogramming.nl${producytLink}`,
 				})}
 			</script>
 
 			{logo}
 			<h1 className="md:text-4xl text-2xl my-0 w-fit mx-auto">{title}</h1>
+
 			<div className="md:my-12 my-6">{description}</div>
 		</div>
 	);
