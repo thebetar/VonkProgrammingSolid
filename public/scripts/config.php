be9d53dc4ce1c62ab1e6ed8b68ff721a131458dc
Lars Vonk <larserikvonk@outlook.com>
2026-04-04 16:28:44 +0200

    chore: config use env variables

--- public/scripts/config.php
+++ public/scripts/config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$DB_SERVER = 'db';
-$DB_NAME = 'test_db';
-$DB_USERNAME = 'admin';
-$DB_PASSWORD = 'admin';
+$DB_SERVER = getenv('DB_SERVER') ?: 'db';
+$DB_NAME = getenv('DB_NAME') ?: 'test_db';
+$DB_USERNAME = getenv('DB_USERNAME') ?: 'admin';
+$DB_PASSWORD = getenv('DB_PASSWORD') ?: 'admin';
 
-$ACCESS_TOKEN = 'e58129e991b9fea196fd601fa6ec9c6eec7a81e3';
+$ACCESS_TOKEN = getenv('ACCESS_TOKEN') ?: 'e58129e991b9fea196fd601fa6ec9c6eec7a81e3';
 ?>
